586805d4014af62fb29068dca6217ab94001860e
andialexandrescu <147060135+andialexandrescu@users.noreply.github.com>
2026-02-18 14:13:20 +0200

    added pocketbase integration

--- workflow-app/features/auth/server/route.ts
+++ workflow-app/features/auth/server/route.ts
@@ -2,15 +2,71 @@ import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 import { loginSchema, registerSchema } from "../schemas";
+import { createAdminClient } from "@/lib/pocketbase";
+import { deleteCookie, setCookie } from "hono/cookie";
 
 const app = new Hono()
     .post("/login", zValidator("json", loginSchema), async (c) => {
-        const { email, password } = c.req.valid("json");
-        return c.json({ email, password });
+        try{
+            const { email, password } = c.req.valid("json");
+
+            const pb = await createAdminClient();
+
+            const session = await pb.collection('users').authWithPassword(email, password);
+
+            setCookie(c, "pb_auth", session.token, {
+                httpOnly: true,
+                path: "/",
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+                secure: process.env.NODE_ENV === "production",
+            });
+
+            return c.json({success: true});
+        } catch (error) {
+            console.error("Register error:", error);
+            if (error instanceof Error && 'response' in error) {
+                console.error("PocketBase response:", (error as any).response);
+            }
+            return c.json({ error: (error as Error).message }, 400);
+        }
     })
     .post("/register", zValidator("json", registerSchema), async (c) => {
-        const { username, email, password } = c.req.valid("json");
-        return c.json({ username, email, password });
+        try{
+            const { username, email, password } = c.req.valid("json");
+
+            const pb = await createAdminClient();
+            
+            const user = await pb.collection('users').create({
+                username,
+                email,
+                password,
+                passwordConfirm: password,
+            });
+
+            const session = await pb.collection('users').authWithPassword(email, password);
+
+            setCookie(c, "pb_auth", session.token, {
+                httpOnly: true,
+                path: "/",
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+                secure: process.env.NODE_ENV === "production",
+            });
+
+            return c.json(user);
+        } catch (error) {
+            console.error("Register error:", error);
+            if (error instanceof Error && 'response' in error) {
+                console.error("PocketBase response:", (error as any).response);
+            }
+            return c.json({ error: (error as Error).message }, 400);
+        }
+    })
+    .post("/logout", (c) => {
+        deleteCookie(c, "pb_auth");
+
+        return c.json({success: true});
     });
 
 export default app;
---
+++ workflow-app/lib/pocketbase.ts
@@ -0,0 +1,22 @@
+import { TypedPocketBase } from '@/types/pocketbase-types';
+import PocketBase from 'pocketbase';
+import 'server-only';
+
+const client = new PocketBase(
+    process.env.NEXT_PUBLIC_POCKETBASE_API_URL
+) as TypedPocketBase;
+
+export const createAdminClient = async () => {
+    if (!process.env.POCKETBASE_ADMIN_EMAIL || !process.env.POCKETBASE_ADMIN_PASSWORD) {
+        throw new Error('Admin credentials not defined');
+    }
+
+    await client.admins.authWithPassword(
+        process.env.POCKETBASE_ADMIN_EMAIL,
+        process.env.POCKETBASE_ADMIN_PASSWORD
+    );
+
+    return client;
+};
+
+export default client;
